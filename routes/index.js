929346533924faee83aa984635a3e42851185a9d
= <=>
2022-05-25 14:45:55 -0300

    Rota de envio de template

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
 var express = require('express');
+
 var router = express.Router();
+
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Active Sender' });
+  res.render('index', { title: 'Active Sender'});
 });
 
-router.post('/', function (req, res, next) {
-  return "ola"
+router.post('/api', function (req, res, next) {
+  let template;
+  if(req.body.template === '1') {
+    template = "Template1"
+  }else if(req.body.template === '1') {
+    template = "Template2"
+  }else {
+    template = "Template3";
+  }
+    
+  axios.post('https://preprod.sapios.com.br/botrouter/api/webhook/rest/199', {
+           from: {
+               "userid": "teste",
+               "name": "user1",
+               "username": "teste"
+           },
+           type: "message",
+           callbackurl: "https://sapios.free.beeceptor.com",
+           text: `+55${req.body.numero},${template}`
+       }, {
+           headers: {
+               'Content-Type': 'application/json',
+               'Authorization': '9f1fddb8-9979-4696-bbb2-3fb4e6cf8eaf'
+           },
+       })
+           .then(function (response) {
+               console.log(response);
+           })
+           .catch(function (error) {
+               console.log(error);
+           });
+           
+  let body = req.body;
+  console.log(body);
 });
 
 module.exports = router;
