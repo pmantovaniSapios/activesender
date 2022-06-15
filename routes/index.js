82d7ec018f64479e1a93b7a24985a98477d4ff29
Pedro Mantovani <pmantovani@sapios.com.br>
2022-06-15 13:00:21 -0300

    ajustes envio

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 var router = express.Router();
 
@@ -6,41 +8,48 @@ var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Active Sender'});
+  res.render('index', { title: 'Active Sender' });
 });
 
 router.post('/api', function (req, res, next) {
+  let uuid = uuidv4();
   let template;
-  if(req.body.template === '1') {
+  if (req.body.template === '1') {
     template = "Template1"
-  }else if(req.body.template === '2') {
+  } else if (req.body.template === '2') {
     template = "Template2"
-  }else {
+  } else {
     template = "Template";
   }
-    
-  axios.post('https://preprod.sapios.com.br/botrouter/api/webhook/rest/199', {
-           from: {
-               "userid": "teste",
-               "name": "user1",
-               "username": "teste"
-           },
-           type: "message",
-           callbackurl: "https://sapios.free.beeceptor.com",
-           text: `+55${req.body.numero},${template}`
-       }, {
-           headers: {
-               'Content-Type': 'application/json',
-               'Authorization': '9f1fddb8-9979-4696-bbb2-3fb4e6cf8eaf'
-           },
-       })
-           .then(function (response) {
-               console.log(response);
-           })
-           .catch(function (error) {
-               console.log(error);
-           });
-           
+
+  axios.post('https://preprod.sapios.com.br/botrouter/api/webhook/rest/267', {
+    from: {
+      "userid": uuid,
+      "name": "Envio HSM SapiosChat - Hapvida",
+      "username": uuid
+    },
+    type: "message",
+    callbackurl: "https://webhook.site/a67badd7-4815-41ec-8bdc-926334196f05",
+    type: "event",
+    totask: "Envio HSM",
+    principaldialog: "start",
+    name: "jumpToTaskEvent",
+    eventdata: {
+      telefoneRest: `${req.body.numero}`
+    }
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': '360f782d-e9e5-436b-8fc6-560b6a4747bd'
+    },
+  })
+    .then(
+      res.redirect("http://localhost:3000/")
+    )
+    .catch(function (error) {
+      console.log(error);
+    });
+
   let body = req.body;
   console.log(body);
 });
